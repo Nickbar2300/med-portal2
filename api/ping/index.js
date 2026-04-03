aae1445578435156dbe2068cd141b90d454b283a
Nickbar2300 <nbarbera@keystonehealthpartners.com>
2026-04-03 12:10:20 -0400

    Update index.js

--- api/ping/index.js
+++ api/ping/index.js
@@ -1,19 +1,24 @@
-module.exports = async function (context, req) {
-  context.log('ping called');
-  context.res = {
-    status: 200,
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      ok: true,
-      node: process.version,
-      runtime: process.env.FUNCTIONS_EXTENSION_VERSION || 'unknown',
-      env_vars: {
-        BB2_CLIENT_ID:      !!process.env.BB2_CLIENT_ID,
-        BB2_CLIENT_SECRET:  !!process.env.BB2_CLIENT_SECRET,
-        ANTHROPIC_API_KEY:  !!process.env.ANTHROPIC_API_KEY,
-        TWILIO_ACCOUNT_SID: !!process.env.TWILIO_ACCOUNT_SID,
-        PORTAL_URL:         !!process.env.PORTAL_URL,
-      }
-    })
-  };
-};
+const { app } = require('@azure/functions');
+
+app.http('ping', {
+  methods: ['GET', 'POST'],
+  authLevel: 'anonymous',
+  handler: async (request, context) => {
+    context.log('ping called');
+    return {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        ok: true,
+        node: process.version,
+        env_vars: {
+          BB2_CLIENT_ID:      !!process.env.BB2_CLIENT_ID,
+          BB2_CLIENT_SECRET:  !!process.env.BB2_CLIENT_SECRET,
+          ANTHROPIC_API_KEY:  !!process.env.ANTHROPIC_API_KEY,
+          TWILIO_ACCOUNT_SID: !!process.env.TWILIO_ACCOUNT_SID,
+          PORTAL_URL:         !!process.env.PORTAL_URL,
+        }
+      })
+    };
+  }
+});
