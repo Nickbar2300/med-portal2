51c036fd91180e7d69b01b2e396520a6a60b16e9
Nickbar2300 <nbarbera@keystonehealthpartners.com>
2026-04-06 10:40:37 -0400

    Update index.js

--- api/ping/index.js
+++ api/ping/index.js
@@ -1,4 +1,25 @@
-module.exports = function (context, req) {
-  context.res = { body: "pong" };
-  context.done();
-};
+const { app } = require('@azure/functions');
+
+app.http('ping', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log('ping called');
+        return {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                ok: true,
+                node: process.version,
+                runtime: 'v4',
+                env_vars: {
+                    BB2_CLIENT_ID:      !!process.env.BB2_CLIENT_ID,
+                    BB2_CLIENT_SECRET:  !!process.env.BB2_CLIENT_SECRET,
+                    ANTHROPIC_API_KEY:  !!process.env.ANTHROPIC_API_KEY,
+                    TWILIO_ACCOUNT_SID: !!process.env.TWILIO_ACCOUNT_SID,
+                    PORTAL_URL:         !!process.env.PORTAL_URL,
+                }
+            })
+        };
+    }
+});
