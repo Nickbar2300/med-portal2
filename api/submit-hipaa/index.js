bbbe011509aac105623bff8760cbf5b545e2c775
Nickbar2300 <nbarbera@keystonehealthpartners.com>
2026-04-06 13:59:03 -0400

    Delete api directory

--- api/submit-hipaa/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function (context, req) {
-  context.res = {
-    status: 200,
-    headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
-    body: JSON.stringify({ success: true, message: 'HIPAA received - storage coming soon' })
-  };
-};
