5acced6f678b23305cbe7d483a34a9b44890040f
Nickbar2300 <nbarbera@keystonehealthpartners.com>
2026-04-06 10:10:33 -0400

    Update index.js

--- api/ping/index.js
+++ api/ping/index.js
@@ -1,17 +1,4 @@
-module.exports = async function (context, req) {
-  context.res = {
-    status: 200,
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      ok: true,
-      node: process.version,
-      env_vars: {
-        BB2_CLIENT_ID:      !!process.env.BB2_CLIENT_ID,
-        BB2_CLIENT_SECRET:  !!process.env.BB2_CLIENT_SECRET,
-        ANTHROPIC_API_KEY:  !!process.env.ANTHROPIC_API_KEY,
-        TWILIO_ACCOUNT_SID: !!process.env.TWILIO_ACCOUNT_SID,
-        PORTAL_URL:         !!process.env.PORTAL_URL,
-      }
-    })
-  };
+module.exports = function (context, req) {
+  context.res = { body: "pong" };
+  context.done();
 };
